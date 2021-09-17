f5c7f0ab0d32a41d7934f55322866c6ed8b5325d
Absolu242 <wangleibniz01@gmail.com>
2021-09-18 04:43:36 +0800

    fixed an import issue

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,48 @@
-
-import Books from "../components/books"
-import Layout from "../components/Layout"
-import Projects from "../components/Projects"
-import { HomeSection } from "../styles/Home.styles"
+import Books from "../components/Books";
+import Layout from "../components/Layout";
+import Projects from "../components/Projects";
+import { HomeSection } from "../styles/Home.styles";
 
 export default function Home() {
   return (
     <Layout>
-      <HomeSection className='about'>
-        
+      <HomeSection className="about">
+        <div className="top">
+          <div className="top-left">
+            <h1>
+              Hi , I'm <span>Rahan</span>
+            </h1>
+            <br />
 
-        <div className='top'>
-<div className='top-left'>
-<h1>Hi , I'm <span>Rahan</span></h1>
-        <br/>
-          
-        <p className='paraph'>
-          I'm a <span>Frontend</span> Web Developer ,with a strong entrepreneurial spirit in
-          love with Web Design and like to <span>learn </span>new things. My Goal is to <span> grow </span> as a developer and then move to higher responsibility roles like project manager  :) !
-          <br /> <br />
-        </p>
-  </div>
-
-  <div className='top-right'>
-
-  
-    </div>
+            <p className="paraph">
+              I'm a <span>Frontend</span> Web Developer ,with a strong
+              entrepreneurial spirit in love with Web Design and like to{" "}
+              <span>learn </span>new things. My Goal is to <span> grow </span>{" "}
+              as a developer and then move to higher responsibility roles like
+              project manager :) !
+              <br /> <br />
+            </p>
           </div>
 
+          <div className="top-right"></div>
+        </div>
+
         <div>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <h2>What I use :</h2>
-          <p className='stack'>
+          <p className="stack">
             HTML, CSS, Sass, React, Nextjs, Redux <br />
-            Node, Express, MongoDB, React Native,<br/>
-            Jest, Git /Github,  Formik, Redux-toolkit
+            Node, Express, MongoDB, React Native,
+            <br />
+            Jest, Git /Github, Formik, Redux-toolkit
           </p>
         </div>
         <br />
-       
+
         <br />
       </HomeSection>
-      <Projects/>
-      <Books/>
+      <Projects />
+      <Books />
     </Layout>
-  )
-}
+  )}
